44405d3cfae96eb6c2d18142edaa1f3ae3a77245
InesIvanova <ines.iv.ivanova@gmail.com>
2017-01-17 21:04:16 +0200

    New code

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,43 +1,18 @@
 ﻿using System;
+using System.Globalization;
 
-class Program
+class HolidaysBetweenTwoDates
 {
     static void Main()
     {
-        Console.WriteLine("Enter Money:");
-
-        decimal money = decimal.Parse(Console.ReadLine());
-
-            if (money>50)
-            {
-                
-                Console.WriteLine("I'll give it to IZdislav");
-                return;
-            } 
-            if (money>=50)
-            {
-                if (money % 4 == 0)
-                {
-                    Console.WriteLine("Vzimame parite za nas");
-                }
-                else
-                {
-                    Console.WriteLine("I'll give it to IZdislav");
-                    return;
-                }
-            } 
-            
-            
-            Console.WriteLine("I don't have enough money. Sorry! :( ;d");
-
-            Console.WriteLine("Izdislav was here");
-
-            bool isHappy = true;
-            
-        
-
-        Console.WriteLine("I don't have enough money. Sorry! :( ;d");
-
+        var startDate = DateTime.ParseExact(Console.ReadLine(),
+            "dd.m.yyyy", CultureInfo.InvariantCulture);
+        var endDate = DateTime.ParseExact(Console.ReadLine(),
+            "dd.m.yyyy", CultureInfo.InvariantCulture);
+        var holidaysCount = 0;
+        for (var date = startDate; date <= endDate; date.AddDays(1))
+            if (date.DayOfWeek == DayOfWeek.Saturday &&
+                date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
+        Console.WriteLine(holidaysCount);
     }
 }
-
