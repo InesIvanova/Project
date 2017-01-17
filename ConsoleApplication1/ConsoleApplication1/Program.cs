477cb170bbcdcbd1a91e80ef5dce590c4322ca2c
InesIvanova <ines.iv.ivanova@gmail.com>
2017-01-17 20:20:30 +0200

    This is the code

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal money = decimal.Parse(Console.ReadLine());
+
+            if (money>=50)
+            {
+                Console.WriteLine("I'll give it to IZdislav");
+                return;
+            } 
+            
+            
+                Console.WriteLine("I don't have enough money. Sorry! :( ;d");
+            
+        }
+    }
+}
