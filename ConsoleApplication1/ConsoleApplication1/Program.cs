bec492035a3dba48d4fb8844ab4042e5643ea64b
pavlinpetko88 <pavlinpetkov@icloud.com>
2017-02-17 06:18:32 +0200

    New 1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,7 +5,6 @@ class HolidaysBetweenTwoDates
 {
     static void Main()
     {
-
         var startDate = DateTime.ParseExact(Console.ReadLine(),
             "dd.m.yyyy", CultureInfo.InvariantCulture);
         var endDate = DateTime.ParseExact(Console.ReadLine(),
@@ -14,7 +13,5 @@ class HolidaysBetweenTwoDates
         for (var date = startDate; date <= endDate; date.AddDays(1))
             if (date.DayOfWeek == DayOfWeek.Saturday &&
                 date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
-        Console.WriteLine(holidaysCount);
-
-    }
+        Console.WriteLine(holidaysCount);    }
 }
